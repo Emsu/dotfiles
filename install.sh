973ee8e9ca705aa2759eb0245c68baf4edd96c02
Michael Su <mdasu1@gmail.com>
2024-12-27 04:37:14 -0800

    chore: update zshrc

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/zsh
 
-if ! $(which antidote) ; then
+if ! command -v brew &> /dev/null; then
+    echo "Installing homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+if ! brew list | grep antidote > /dev/null 2>&1 ; then
     echo "Installing antidote..."
     brew install antidote
 fi
@@ -8,6 +13,16 @@ fi
 if ! command -v stow > /dev/null 2>&1; then
     echo "Installing stow..."
     brew install stow
+fi
+
+if ! command -v starship > /dev/null 2>&1; then
+    echo "Installing starship..."
+    curl -sS https://starship.rs/install.sh | sh
+fi
+
+if ! command -v mise > /dev/null 2>&1; then
+    echo "Installing mise..."
+    curl https://mise.run | sh
 fi
 
 stow .
