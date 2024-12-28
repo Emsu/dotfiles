00127b466219d61afa13af6db484995525103102
Michael Su <mdasu1@gmail.com>
2024-12-27 23:53:09 -0800

    chore: add ghostty config and updated install

--- install.sh
+++ install.sh
@@ -22,7 +22,39 @@ fi
 
 if ! command -v mise > /dev/null 2>&1; then
     echo "Installing mise..."
+    brew install zstd
     curl https://mise.run | sh
 fi
 
+if ! command -v mise > /dev/null 2>&1; then
+    echo "Installing ghostty..."
+    brew install ghostty
+fi
+
+if ! command -v nvim > /dev/null 2>&1; then
+    echo "Installing nvim..."
+    brew install nvim
+fi
+
+if ! command -v mise > /dev/null 2>&1; then
+    echo "Installing fzf..."
+    brew install fzf
+fi
+
+if ! command -v mise > /dev/null 2>&1; then
+    echo "Installing zoxide..."
+    brew install zoxide
+fi
+
 stow .
+
+# check gitconfig is setup
+if [ -z "$(git config --global user.name)" ] || [ -z "$(git config --global user.email)" ]; then
+    echo "Git configuration is incomplete. Please set up your identity."
+    # read -p "Enter your name: " git_name
+    # read -p "Enter your email: " git_email
+    echo -n "Enter your name: " && read git_name
+    echo -n "Enter your email: " && read git_email
+    git config --global user.name "$git_name"
+    git config --global user.email "$git_email"
+fi
